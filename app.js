cb49700f8b36b99a9c54afa87922f21eee3549eb
David Marshall <david.marshall@carmigo.io>
2022-04-29 10:22:54 -0500

    this is the part of deployment i'm not good at

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const routes = require('./router');
 
 const hostname = "127.0.0.1";
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 
